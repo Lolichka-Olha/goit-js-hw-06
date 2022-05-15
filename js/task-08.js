807b345f053c81998bdbd293c368618bc00f0ac0
Olha Truba <o.ljolik@gmail.com>
2022-05-15 21:14:31 +0300

    adds task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,19 @@
+const form = document.querySelector(".login-form");
 
+function submitHandler(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("Введіть будь ласка всі дані!");
+  }
+
+  console.log({ email: email.value, password: password.value });
+
+  event.currentTarget.reset();
+}
+
+form.addEventListener("submit", submitHandler);
