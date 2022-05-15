076fd50764b3ea5a7adf71059f40f98dc0148d18
Olha Truba <o.ljolik@gmail.com>
2022-05-15 21:55:01 +0300

    adds task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
+const bodyColor = document.querySelector("body");
+const btnColor = document.querySelector(".change-color");
+const textColor = document.querySelector(".color");
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+function changeColor() {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  textColor.textContent = getRandomHexColor();
+}
+
+btnColor.addEventListener("click", changeColor);
